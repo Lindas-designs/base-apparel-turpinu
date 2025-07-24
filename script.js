33aca789d756affc20841a862d68ee05b77bea10
Linda <kazusalinda@gmail.com>
2025-07-24 14:48:20 +0300

    script added

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+const input = document.querySelector(".input");
+const errImage = document.querySelector(".error-img");
+const button = document.querySelector(".btn");
+const errMessage = document.querySelector(".error-message");
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  let inputText = input.value;
+  console.log(inputText);
+  inputText = "";
+
+  /*
+  if (!inputText.includes("@")) {
+    errImage.classList.remove("hidden") ||
+      errMessage.classList.remove("hidden");
+  } else if (
+    inputText.includes("@") ||
+    !errImage.classList.contains("hidden")
+  ) {
+    errImage.classList.add("hidden") || errMessage.classList.add("hidden");
+    //  ||
+    // (inputText = "");
+  }
+
+  // else if (inputText.includes("@") || errImage.classList.contains("hidden")) {
+  //   inputText = "";
+  // }*/
+});
